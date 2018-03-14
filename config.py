28b6fa2e43133ee5d04e6fdee58ed347271e589a
Alex Chetverov <alexu-che@yandex.ru>
2018-03-14 19:34:34 +0300

    Хуки, хероку, весрия 0.12 без ключа

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-token = '547635609:AAFNXFPbRsxthnAiM38kavF0Fg-Lc8SIFqQ' # Токен бота
+token = '' # Токен бота
 database_name = 'db/inf9gia.db'  # Файл с базой данных
 shelve_name = 'db/shelve'  # Файл с хранилищем
 ver='0.12 (сборка 14032108_12)'
