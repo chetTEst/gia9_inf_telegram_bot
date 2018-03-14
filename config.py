e63deb494a6da4a7d2141ac6b2a18f2c1d139508
Alex Chetverov <alexu-che@yandex.ru>
2018-03-14 11:17:44 +0300

    на Хероку

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
-token = '' # Токен бота
+token = '547635609:AAFNXFPbRsxthnAiM38kavF0Fg-Lc8SIFqQ' # Токен бота
 database_name = 'db/inf9gia.db'  # Файл с базой данных
 shelve_name = 'db/shelve'  # Файл с хранилищем
